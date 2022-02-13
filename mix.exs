ddb2eb07406a23d438426885e7c55f0336cb6329
Tosin <me@tosinsoremekun.com>
2022-02-13 19:28:03 +0200

    chore: update mix version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Paystack.MixProject do
   def project do
     [
       app: :paystack,
-      version: "0.5.2",
+      version: "0.6.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
