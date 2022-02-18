70eb27b2a048b2de2aaa50ce7069a5068c2fefd4
Tosin Soremekun <me@tosinsoremekun.com>
2022-02-18 13:49:00 +0200

    feat: add apple pay api's (#15)

---
+++ lib/paystack/apple_pay.ex
@@ -0,0 +1,29 @@
+defmodule Paystack.ApplePay do
+  import Paystack.Helpers, only: [paystack: 0]
+
+  @moduledoc """
+  The Apple Pay API allows you register your application's top-level domain or subdomain
+  """
+
+  @type paystack_response() :: {:ok, Paystack.Response.t()} | {:error, any}
+
+  @doc """
+  Register a top-level domain or subdomain for your Apple Pay integration.
+  """
+  @spec register_domain(String.t) :: paystack_response()
+  def register_domain(domain_name),
+    do: paystack().post("/apple-pay/domain", %{ domainName: domain_name })
+
+  @doc """
+  Lists all registered domains on your integration. Returns an empty array if no domains have been added.
+  """
+  @spec list_domains :: paystack_response()
+  def list_domains(), do: paystack().get("/apple-pay/domain")
+
+  @doc """
+  Unregister a top-level domain or subdomain previously used for your Apple Pay integration.
+  """
+  @spec unregister_domain(String.t) :: paystack_response()
+  def unregister_domain(domain_name),
+    do: paystack().delete("/apple-pay/domain", %{ domainName: domain_name })
+end
